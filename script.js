b9c6d2ca9ef6d3e02b748b4e686b262c1f4d2a33
NitroCodes1 <ttnirushan@gmail.com>
2024-08-28 15:21:38 +0100

    Bracket error corrected on computer choice funtion

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function getComputerChoice {
+function getComputerChoice{
+    
     const option = ["Rock", "Paper", "Scissors"];
     const randomIndex = Math.floor(Math.random() * 3);
-    const randomOption = option[randomIndex]
+    const randomOptions = option[randomIndex]
+    console.log(randomOptions)
 }
+    
