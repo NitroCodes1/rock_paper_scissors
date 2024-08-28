99dc5d7c1aafb0094c16f434c414c37f6a59e49e
NitroCodes1 <ttnirushan@gmail.com>
2024-08-28 15:15:46 +0100

    Computer random choice generated

---
+++ script.js
@@ -0,0 +1,5 @@
+function getComputerChoice {
+    const option = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * 3);
+    const randomOption = option[randomIndex]
+}
